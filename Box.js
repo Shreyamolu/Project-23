0f124da133b4785547b1cd8b323ed2adcb851e8c
Shreyamolu <74354983+Shreyamolu@users.noreply.github.com>
2020-11-26 23:14:37 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,25 @@
+/*class Box {
+
+    constructor(x,y,width,height){
+        
+
+
+    }
+
+
+
+
+
+
+}*/
+
+
+/*
+line1 = Bodies.rectangle(300, 650, 100, 20 , {isStatic:true} );
+World.add(world, line1);
+
+line2 = Bodies.rectangle(300, 650, 20, 200 , {isStatic:true} );
+World.add(world, line2);
+
+line3 = Bodies.rectangle(500, 650, 100, 20 , {isStatic:true} );
+World.add(world, line3);*/
